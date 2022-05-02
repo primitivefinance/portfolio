1e26b9c9b80f0ef4cc85d1f3c7900b861e5543b7
clemlak <clementlakhal@gmail.com>
2022-05-02 21:43:06 +0400

    build: change Solidity to 0.8.13

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,13 +6,7 @@ import '@primitivefi/hardhat-dodoc'
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: '0.8.6',
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 400,
-      },
-    },
+    version: '0.8.13',
   },
 }
 
