4464c12a822e0b35000b3b26a5b104733b9377d4
clemlak <clementlakhal@gmail.com>
2022-05-04 19:23:19 +0400

    feat: add DecimalsError, Pool struct, etc

--- contracts/PrimitiveHyper.sol
+++ contracts/PrimitiveHyper.sol
@@ -2,7 +2,23 @@
 pragma solidity 0.8.13;
 
 contract PrimitiveHyper {
-    function deploy() external {
+    error DecimalsError();
+
+    struct Pool {
+        address risky;
+        address stable;
+        uint128 strike;
+        uint32 sigma;
+        uint8 riskyDecimals;
+        uint8 stableDecimals;
+    }
+
+    Pool[] public pools;
+
+    function create(
+        address risky,
+        address stable
+    ) external {
 
     }
 }
