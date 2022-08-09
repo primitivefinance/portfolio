bb1c55ed239baff592ecf2e085d0d19992d5f5b1
alexangelj <alexangel.j@gmail.com>
2022-08-09 16:05:11 -0700

    refactor(interfaces): fix unsaved file

--- contracts/interfaces/proofOfConcept/enigma/IEnigmaDataStructuresPoC.sol
+++ contracts/interfaces/proofOfConcept/enigma/IEnigmaDataStructuresPoC.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.13;
 
 /// @title IEngimaDataStructures
 /// @dev All the structs used by the Enigma and its higher level contracts. Returned by the mapping public getters.
-interface IEnigmaDataStructures {
+interface IEnigmaDataStructuresPoC {
     // --- Immutable --- //
 
     /// @dev Immutable curve parameters.
