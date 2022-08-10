069ff1fb9f20ca0fc7a7602ce4f286079cf4b1c6
alexangelj <alexangel.j@gmail.com>
2022-08-10 16:44:41 -0700

    feat(createPair): implements _createPair function in Hyper

--- contracts/prototype/HyperPrototype.sol
+++ contracts/prototype/HyperPrototype.sol
@@ -28,5 +28,52 @@ abstract contract HyperPrototype is EnigmaVirtualMachinePrototype {
 
     function _createCurve(bytes calldata data) internal {}
 
-    function _createPair(bytes calldata data) internal {}
+    /**
+     * @notice Maps a nonce to a pair of token addresses and their decimal places.
+     * @dev Pairs are used in pool creation to determine the pool's underlying tokens.
+     *
+     * @custom:reverts If decoded addresses are the same.
+     * @custom:reverts If __ordered__ pair of addresses has already been created and has a non-zero pairId.
+     * @custom:reverts If decimals of either token are not between 6 and 18, inclusive.
+     */
+    function _createPair(bytes calldata data) internal returns (uint16 pairId) {
+        (address asset, address quote) = Instructions.decodeCreatePair(data); // Expects Engima encoded data.
+        if (asset == quote) revert SameTokenError();
+
+        pairId = _getPairId[asset][quote];
+        if (pairId != 0) revert PairExists(pairId);
+
+        (uint8 assetDecimals, uint8 quoteDecimals) = (IERC20(asset).decimals(), IERC20(quote).decimals());
+
+        if (!_isValidDecimals(assetDecimals)) revert DecimalsError(assetDecimals);
+        if (!_isValidDecimals(quoteDecimals)) revert DecimalsError(quoteDecimals);
+
+        unchecked {
+            pairId = uint16(++_pairNonce); // Increments the pair nonce, returning the nonce for this pair.
+        }
+
+        // Writes the pairId into a fetchable mapping using its tokens.
+        _getPairId[asset][quote] = pairId; // note: No reverse lookup, because order matters!
+
+        // Writes the pair into Enigma state.
+        _pairs[pairId] = Pair({
+            tokenBase: asset,
+            decimalsBase: assetDecimals,
+            tokenQuote: quote,
+            decimalsQuote: quoteDecimals
+        });
+
+        emit CreatePair(pairId, asset, quote);
+    }
+
+    function _isValidDecimals(uint8 decimals) internal view returns (bool valid) {
+        assembly {
+            // Is `amount` between 6 and 18 decimals?
+            function isValid(amount) -> between {
+                between := iszero(sgt(mul(sub(amount, 6), sub(amount, 18)), 0)) // iszero(x > amount ? 1 : 0) ? true : false, (n - a) * (n - b) <= 0, n = amount, a = lower, b = upper
+            }
+
+            valid := isValid(decimals)
+        }
+    }
 }
