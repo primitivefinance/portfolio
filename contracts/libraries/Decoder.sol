5dac0792cc64c61d0ed32ccb2447056212afa2de
clemlak <clementlakhal@gmail.com>
2022-09-30 18:04:13 +0400

    feat: reorg Decoder, Encoder, Utils, Instructions

--- contracts/libraries/Decoder.sol
+++ contracts/libraries/Decoder.sol
@@ -1,77 +1,199 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity 0.8.13;
 
+import "./Utils.sol";
+
 /// @title    Decoder Library
 /// @dev      Solidity library to manipulate and covert bytes
 /// @author   Primitive
 library Decoder {
-    /// @dev            Separates the nibbles of a byte into two bytes
-    /// @param data     Byte to separate
-    /// @return upper   Upper nibble
-    /// @return lower   Lower nibble
-    function separate(bytes1 data) internal pure returns (bytes1 upper, bytes1 lower) {
-        upper = data >> 4;
-        lower = data & 0x0f;
+    // --- Errors --- //
+    error DecodePairBytesLength(uint256 expected, uint256 length);
+
+    /// @dev Expects the standard instruction with two trailing run-length encoded amounts.
+    /// @param data Maximum 8 + 3 + 3 + 16 + 16 = 46 bytes.
+    /// | 0x | 1 packed byte useMax Flag - enigma code | 6 byte poolId | 3 byte loTick | 3 byte hiTick | 1 byte pointer to next power byte | 1 byte power | ...amount | 1 byte power | ...amount |
+    function decodeAddLiquidity(bytes calldata data)
+        internal
+        pure
+        returns (
+            uint8 useMax,
+            uint48 poolId,
+            int24 loTick,
+            int24 hiTick,
+            uint128 deltaLiquidity
+        )
+    {
+        (bytes1 maxFlag, ) = separate(data[0]);
+        useMax = uint8(maxFlag);
+        poolId = uint48(bytes6(data[1:7]));
+        loTick = int24(uint24(bytes3(data[7:10])));
+        hiTick = int24(uint24(bytes3(data[10:13])));
+        deltaLiquidity = toAmount(data[13:]);
+        //uint8 pointer = uint8(data[13]);
+        //deltaBase = toAmount(data[14:pointer]);
+        //deltaQuote = toAmount(data[pointer:]);
     }
 
-    function pack(bytes1 upper, bytes1 lower) internal pure returns (bytes1 data) {
-        data = (upper << 4) | lower;
+    /// @notice The pool swap fee is a parameter, which is store and then used to calculate `gamma`.
+    /// @dev Expects a 27 length byte array of left padded parameters.
+    /// @param data Maximum 1 + 3 + 4 + 2 + 2 + 16 = 28 bytes.
+    /// | 0x | 1 byte enigma code | 3 bytes sigma | 4 bytes maturity | 2 bytes fee | 2 bytes priority fee | 16 bytes strike |
+    function decodeCreateCurve(bytes calldata data)
+        internal
+        pure
+        returns (
+            uint24 sigma,
+            uint32 maturity,
+            uint16 fee,
+            uint16 priorityFee,
+            uint128 strike
+        )
+    {
+        require(data.length < 32, "Curve data too long");
+        sigma = uint24(bytes3(data[1:4])); // note: First byte is the create pair ecode.
+        maturity = uint32(bytes4(data[4:8]));
+        fee = uint16(bytes2(data[8:10]));
+        priorityFee = uint16(bytes2(data[10:12]));
+        strike = uint128(bytes16(data[12:]));
     }
 
-    /// @dev           Converts an array of bytes into a byte32
-    /// @param raw     Array of bytes to convert
-    /// @return data   Converted data
-    function toBytes32(bytes memory raw) internal pure returns (bytes32 data) {
-        assembly {
-            data := mload(add(raw, 32))
-            let shift := mul(sub(32, mload(raw)), 8)
-            data := shr(shift, data)
-        }
+    /// @dev Expects a 41-byte length array with two addresses packed into it.
+    /// @param data Maximum 1 + 20 + 20 = 41 bytes.
+    /// | 0x | 1 byte enigma code | 20 bytes base token | 20 bytes quote token |.
+    function decodeCreatePair(bytes calldata data) internal pure returns (address tokenBase, address tokenQuote) {
+        if (data.length != 41) revert DecodePairBytesLength(41, data.length);
+        tokenBase = address(bytes20(data[1:21])); // note: First byte is the create pair ecode.
+        tokenQuote = address(bytes20(data[21:]));
     }
 
-    /// @dev           Converts an array of bytes into a bytes16.
-    /// @param raw     Array of bytes to convert.
-    /// @return data   Converted data.
-    function toBytes16(bytes memory raw) internal pure returns (bytes16 data) {
-        assembly {
-            data := mload(add(raw, 32))
-            let shift := mul(sub(16, mload(raw)), 8)
-            data := shr(shift, data)
-        }
+    /// @dev Expects a poolId and one left zero padded amount for `price`.
+    /// @param data Maximum 1 + 6 + 16 = 23 bytes.
+    /// | 0x | 1 byte enigma code | left-pad 6 bytes poolId | left-pad 16 bytes |
+    function decodeCreatePool(bytes calldata data)
+        internal
+        pure
+        returns (
+            uint48 poolId,
+            uint16 pairId,
+            uint32 curveId,
+            uint128 price
+        )
+    {
+        poolId = uint48(bytes6(data[1:7])); // note: First byte is the create pool ecode.
+        pairId = uint16(bytes2(data[1:3]));
+        curveId = uint32(bytes4(data[3:7]));
+        price = uint128(bytes16(data[7:23]));
     }
 
-    /// @dev             Converts an array of bytes into an uint128, the array must adhere
-    ///                  to the the following format:
-    ///                  - First byte: Amount of trailing zeros.
-    ///                  - Rest of the array: A hexadecimal number.
-    /// @param raw       Array of bytes to convert.
-    /// @return amount   Converted amount.
-    function toAmount(bytes calldata raw) internal pure returns (uint128 amount) {
-        uint8 power = uint8(raw[0]);
-        amount = uint128(toBytes16(raw[1:raw.length]));
-        amount = amount * uint128(10**power);
+    /// @dev Expects a 6 byte left-pad `poolId`.
+    /// @param data Maximum 6 bytes. | 0x | left-pad 6 bytes poolId |
+    /// Pool id is a packed pair and curve id: | 0x | left-pad 2 bytes pairId | left-pad 4 bytes curveId |
+    function decodePoolId(bytes calldata data)
+        internal
+        pure
+        returns (
+            uint48 poolId,
+            uint16 pairId,
+            uint32 curveId
+        )
+    {
+        poolId = uint48(bytes6(data));
+        pairId = uint16(bytes2(data[:2]));
+        curveId = uint32(bytes4(data[2:]));
     }
 
-    function runLengthEncode(uint256 amount) internal pure returns (uint256 encoded) {
-        encoded = (countEndZeroes(amount));
+    /// @dev Expects an enigma code, poolId, and trailing run-length encoded amount.
+    /// @param data Maximum 1 + 6 + 3 + 3 + 16 = 29 bytes.
+    /// | 0x | 1 packed byte useMax Flag - enigma code | 6 byte poolId | 3 byte loTick index | 3 byte hiTick index | 1 byte amount power | amount in amount length bytes |.
+    function decodeRemoveLiquidity(bytes calldata data)
+        internal
+        pure
+        returns (
+            uint8 useMax,
+            uint48 poolId,
+            uint16 pairId,
+            int24 loTick,
+            int24 hiTick,
+            uint128 deltaLiquidity
+        )
+    {
+        useMax = uint8(data[0] >> 4);
+        pairId = uint16(bytes2(data[1:3]));
+        poolId = uint48(bytes6(data[1:7]));
+        loTick = int24(uint24(bytes3(data[7:10])));
+        hiTick = int24(uint24(bytes3(data[10:13])));
+        deltaLiquidity = uint128(toAmount(data[13:]));
     }
 
-    function countEndZeroes(uint256 amount) internal pure returns (uint256) {
-        uint256 n;
-
-        while (amount % 10 == 0) {
-            n++;
-            amount = amount / 10;
-        }
-
-        return n;
+    /// @notice Swap direction: 0 = base token to quote token, 1 = quote token to base token.
+    /// @dev Expects standard instructions with the end byte specifying swap direction.
+    /// @param data Maximum 1 + 6 + 16 + 1 = 24 bytes.
+    /// | 0x | 1 byte packed flag-enigma code | 6 byte poolId | up to 16 byte TRLE amount | 1 byte direction |.
+    function decodeSwap(bytes calldata data)
+        internal
+        pure
+        returns (
+            uint8 useMax,
+            uint48 poolId,
+            uint128 input,
+            uint128 limit,
+            uint8 direction
+        )
+    {
+        useMax = uint8(data[0] >> 4);
+        poolId = uint48(bytes6(data[1:7]));
+        uint8 pointer = uint8(data[7]);
+        input = uint128(toAmount(data[8:pointer]));
+        limit = uint128(toAmount(data[pointer:data.length - 1])); // note: Up to but not including last byte.
+        direction = uint8(data[data.length - 1]);
     }
 
-    function removeZeroes(uint256 amount) internal pure returns (uint256) {
-        while (amount % 10 == 0) {
-            amount /= 10;
-        }
+    /// @dev Expects an enigma code and positionId.
+    /// @param data Maximum 1 + 12 = 13 bytes.
+    /// | 0x | 1 packed byte useMax Flag - enigma code | 12 byte positionId |.
+    function decodeStakePosition(bytes calldata data) internal pure returns (uint48 poolId, uint96 positionId) {
+        poolId = uint48(bytes6(data[1:7]));
+        positionId = uint96(bytes12(data[1:13]));
+    }
 
-        return amount;
+    /// @dev Expects an enigma code and positionId.
+    /// @param data Maximum 1 + 12 = 13 bytes.
+    /// | 0x | 1 packed byte useMax Flag - enigma code | 12 byte positionId |.
+    function decodeUnstakePosition(bytes calldata data) internal pure returns (uint48 poolId, uint96 positionId) {
+        poolId = uint48(bytes6(data[1:7]));
+        positionId = uint96(bytes12(data[1:13]));
+    }
+
+    /// @param data Maximum 1 + 6 + 20 + 8 + 8 = 43 bytes.
+    /// | 0x | 1 byte enigma code | 6 byte poolId | 20 bytes winner | 8 byte power | 8 byte amount|.
+    function decodeFillPriorityAuction(bytes calldata data)
+        internal
+        pure
+        returns (
+            uint48 poolId,
+            address priorityOwner,
+            uint128 limitOwner
+        )
+    {
+        poolId = uint48(bytes6(data[1:7]));
+        priorityOwner = address(bytes20(data[7:27]));
+        limitOwner = uint128(toAmount(data[28:]));
+    }
+
+    /// | 1 byte pointer to next power byte | 1 byte power | ...amount | 1 byte power | ...amount |
+    function decodeCollectFees(bytes calldata data)
+        internal
+        pure
+        returns (
+            uint96 positionId,
+            uint128 amountAssetRequested,
+            uint128 amountQuoteRequested
+        )
+    {
+        positionId = uint96(bytes12(data[1:13]));
+        uint8 pointer = uint8(data[13]);
+        amountAssetRequested = uint128(toAmount(data[14:pointer]));
+        amountQuoteRequested = uint128(toAmount(data[pointer:data.length]));
     }
 }
--- contracts/libraries/Utils.sol
+++ contracts/libraries/Utils.sol
@@ -41,3 +41,73 @@ function _between(
         valid := isValid(value, lower, upper)
     }
 }
+
+/// @dev            Separates the nibbles of a byte into two bytes
+/// @param data     Byte to separate
+/// @return upper   Upper nibble
+/// @return lower   Lower nibble
+function separate(bytes1 data) pure returns (bytes1 upper, bytes1 lower) {
+    upper = data >> 4;
+    lower = data & 0x0f;
+}
+
+function pack(bytes1 upper, bytes1 lower) pure returns (bytes1 data) {
+    data = (upper << 4) | lower;
+}
+
+/// @dev           Converts an array of bytes into a byte32
+/// @param raw     Array of bytes to convert
+/// @return data   Converted data
+function toBytes32(bytes memory raw) pure returns (bytes32 data) {
+    assembly {
+        data := mload(add(raw, 32))
+        let shift := mul(sub(32, mload(raw)), 8)
+        data := shr(shift, data)
+    }
+}
+
+/// @dev           Converts an array of bytes into a bytes16.
+/// @param raw     Array of bytes to convert.
+/// @return data   Converted data.
+function toBytes16(bytes memory raw) pure returns (bytes16 data) {
+    assembly {
+        data := mload(add(raw, 32))
+        let shift := mul(sub(16, mload(raw)), 8)
+        data := shr(shift, data)
+    }
+}
+
+/// @dev             Converts an array of bytes into an uint128, the array must adhere
+///                  to the the following format:
+///                  - First byte: Amount of trailing zeros.
+///                  - Rest of the array: A hexadecimal number.
+/// @param raw       Array of bytes to convert.
+/// @return amount   Converted amount.
+function toAmount(bytes calldata raw) pure returns (uint128 amount) {
+    uint8 power = uint8(raw[0]);
+    amount = uint128(toBytes16(raw[1:raw.length]));
+    amount = amount * uint128(10**power);
+}
+
+function runLengthEncode(uint256 amount) pure returns (uint256 encoded) {
+    encoded = (countEndZeroes(amount));
+}
+
+function countEndZeroes(uint256 amount) pure returns (uint256) {
+    uint256 n;
+
+    while (amount % 10 == 0) {
+        n++;
+        amount = amount / 10;
+    }
+
+    return n;
+}
+
+function removeZeroes(uint256 amount) pure returns (uint256) {
+    while (amount % 10 == 0) {
+        amount /= 10;
+    }
+
+    return amount;
+}
