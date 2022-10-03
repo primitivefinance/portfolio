45124003edbc607375cabf25aa73bec654ff0733
clemlak <clementlakhal@gmail.com>
2022-10-03 11:21:50 +0400

    feat: add DRAW and FUND instructions

--- contracts/libraries/Instructions.sol
+++ contracts/libraries/Instructions.sol
@@ -5,7 +5,10 @@ pragma solidity 0.8.13;
 /// @dev Contains all the instructions of Hyper
 /// @author Primitive
 library Instructions {
+    // TODO: Maybe change the order of the instructions?
     bytes1 public constant UNKNOWN = 0x00;
+    bytes1 public constant DRAW = 0x02;
+    bytes1 public constant FUND = 0x09;
     bytes1 public constant ADD_LIQUIDITY = 0x01;
     bytes1 public constant REMOVE_LIQUIDITY = 0x03;
     bytes1 public constant COLLECT_FEES = 0x04;
