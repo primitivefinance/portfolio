2503365e6dd643f1c622e28a729b726f5624e336
clemlak <clementlakhal@gmail.com>
2022-10-14 16:35:51 +0400

    feat: add HyperWrapper, testCreatePool test

--- test/foundry/Hyper.t.sol
+++ test/foundry/Hyper.t.sol
@@ -5,18 +5,40 @@ import {WETH} from "solmate/tokens/WETH.sol";
 import "forge-std/Test.sol";
 import "forge-std/console2.sol";
 
+import "../../contracts/test/TestERC20.sol";
 import "../../contracts/Hyper.sol";
+import "../../contracts/libraries/Encoder.sol";
+
+contract HyperWrapper is Hyper {
+    constructor(address WETH_) Hyper(WETH_) {}
+
+    function process(bytes calldata data) external {
+        _process(data);
+    }
+}
 
 contract TestHyper is Test {
+    TestERC20 public token0;
+    TestERC20 public token1;
+
     WETH public weth;
-    Hyper public hyper;
+    HyperWrapper public hyper;
 
     function setUp() public {
+        token0 = new TestERC20("TestERC20", "TEST", 18);
+        token1 = new TestERC20("TestERC20", "TEST", 18);
+
         weth = new WETH();
-        hyper = new Hyper(address(weth));
+        hyper = new HyperWrapper(address(weth));
     }
 
     function testWeth() public {
         assertEq(hyper.WETH(), address(weth));
+    }
+
+    function testCreatePool() public {
+        bytes memory data = Encoder.encodeCreatePool(address(token0), address(token1), hex"0101");
+
+        hyper.process(data);
     }
 }
