d6b9086c643a6aae76aa260a2d75bc3d18b7e6f5
clemlak <clementlakhal@gmail.com>
2022-10-17 14:34:42 +0400

    build: disable optimizer in Hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -34,7 +34,7 @@ const config: HardhatUserConfig = {
         settings: {
           viaIR: false,
           optimizer: {
-            enabled: true,
+            enabled: false,
             runs: 15000,
           },
         },
