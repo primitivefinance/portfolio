1a8d9c3baa0e298fe3bfb3cb8597a90a7c2d68cd
clemlak <clementlakhal@gmail.com>
2022-10-26 17:17:10 +0400

    feat: add getSlotFromPrice

--- contracts/libraries/BrainMath.sol
+++ contracts/libraries/BrainMath.sol
@@ -3,4 +3,18 @@ pragma solidity 0.8.13;
 
 import "solmate/utils/FixedPointMathLib.sol";
 
-library BrainMath {}
+library BrainMath {
+    /**
+     * @dev Returns the slot index associated with a price
+     * @param price Price associated with the slot (with 10^18 precision)
+     * @param a Tick spacing (with 10^18 precision)
+     * @return Associated slot index
+     */
+    function getSlotFromPrice(int256 price, int256 a) internal pure returns (uint256) {
+        return
+            (FixedPointMathLib.divWadDown(
+                uint256(FixedPointMathLib.lnWad(price)),
+                uint256(FixedPointMathLib.lnWad(a))
+            ) + 500000000000000000) / 1 ether;
+    }
+}
