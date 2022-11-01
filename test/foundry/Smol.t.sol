8f308706e3dad9f35536f1527ec81972dee916fe
clemlak <clementlakhal@gmail.com>
2022-11-01 13:39:26 +0400

    test: add tests for Smol contract

--- test/foundry/Smol.t.sol
+++ test/foundry/Smol.t.sol
@@ -10,4 +10,26 @@ contract TestSmol is Test {
     function setUp() public {
         smol = new Smol();
     }
+
+    function test_initiatePool() public {
+        smol.initiatePool(address(0xbeef), address(0xbabe), 0, 0);
+    }
+
+    function test_add_liquidity_above_current_slot() public {
+        smol.initiatePool(address(0xbeef), address(0xbabe), 10, 10);
+
+        smol.addLiquidity(address(0xbeef), address(0xbabe), 11, 12, 1);
+    }
+
+    function test_add_liquidity_including_current_slot() public {
+        smol.initiatePool(address(0xbeef), address(0xbabe), 1300 ether, 10);
+
+        smol.addLiquidity(address(0xbeef), address(0xbabe), 9, 11, 1000000000000000000);
+    }
+
+    function test_add_liquidity_below_current_slot() public {
+        smol.initiatePool(address(0xbeef), address(0xbabe), 10, 10);
+
+        smol.addLiquidity(address(0xbeef), address(0xbabe), 8, 9, 1);
+    }
 }
