9112041463b9b03440d95e6b1243eb814c90ac28
clemlak <clementlakhal@gmail.com>
2022-11-15 23:49:13 +0400

    feat: add _calculateLiquidityDeltas function

--- contracts/libraries/BrainMath.sol
+++ contracts/libraries/BrainMath.sol
@@ -7,8 +7,8 @@ import "@prb/math/contracts/PRBMathUD60x18.sol";
 
 /// @dev Get the price square root using the slot index
 ///      $$p(i)=1.0001^i$$
-function _getPriceSqrtAtSlot(int256 aFixedPoint, int128 slotIndex) pure returns (uint256) {
-    return uint256(PRBMathSD59x18.pow(aFixedPoint, int256(PRBMathSD59x18.div(PRBMathSD59x18.abs(slotIndex), 2))));
+function _getPriceSqrtAtSlot(uint256 aFixedPoint, int128 slotIndex) pure returns (uint256) {
+    return uint256(PRBMathUD60x18.pow(aFixedPoint, PRBMathUD60x18.div(uint256(PRBMathSD59x18.abs(slotIndex)), 2)));
 }
 
 /// @dev Get the slot index using price square root
@@ -16,37 +16,47 @@ function _getPriceSqrtAtSlot(int256 aFixedPoint, int128 slotIndex) pure returns 
 function _getSlotAtPriceSqrt(uint256 aFixedPoint, uint256 priceSqrtFixedPoint) pure returns (int128) {
     return
         int128(
-            PRBMathSD59x18.mul(
-                PRBMathSD59x18.log10(PRBMathSD59x18.sqrt(int256(aFixedPoint))),
-                PRBMathSD59x18.sqrt(int256(priceSqrtFixedPoint))
+            uint128(
+                PRBMathUD60x18.mul(
+                    PRBMathUD60x18.log10(PRBMathUD60x18.sqrt(aFixedPoint)),
+                    PRBMathUD60x18.sqrt(priceSqrtFixedPoint)
+                )
             )
         );
 }
 
-function _calculateDeltaX(
+function _calculateLiquidityDeltas(
+    uint256 aFixedPoint,
     uint256 liquidityDeltaFixedPoint,
-    uint256 priceSqrtAFixedPoint,
-    uint256 priceSqrtBFixedPoint
-) pure returns (uint256) {
-    (priceSqrtAFixedPoint, priceSqrtBFixedPoint) = priceSqrtAFixedPoint < priceSqrtBFixedPoint
-        ? (priceSqrtAFixedPoint, priceSqrtBFixedPoint)
-        : (priceSqrtBFixedPoint, priceSqrtAFixedPoint);
+    uint256 priceSqrtCurrentSlotFixedPoint,
+    int128 currentSlotIndex,
+    int128 lowerSlotIndex,
+    int128 upperSlotIndex
+) pure returns (uint256 amountA, uint256 amountB) {
+    uint256 priceSqrtUpperSlotFixedPoint = _getPriceSqrtAtSlot(aFixedPoint, upperSlotIndex);
+    uint256 priceSqrtLowerSlotFixedPoint = _getPriceSqrtAtSlot(aFixedPoint, lowerSlotIndex);
 
-    return
-        PRBMathUD60x18.div(
-            PRBMathUD60x18.mul(liquidityDeltaFixedPoint, priceSqrtBFixedPoint - priceSqrtAFixedPoint),
-            PRBMathUD60x18.mul(priceSqrtBFixedPoint, priceSqrtAFixedPoint)
+    if (currentSlotIndex < lowerSlotIndex) {
+        amountA = PRBMathUD60x18.mul(
+            liquidityDeltaFixedPoint,
+            PRBMathUD60x18.div(PRBMathUD60x18.toUint(1), priceSqrtLowerSlotFixedPoint) -
+                PRBMathUD60x18.div(PRBMathUD60x18.toUint(1), priceSqrtUpperSlotFixedPoint)
         );
-}
+    } else if (currentSlotIndex < upperSlotIndex) {
+        amountA = PRBMathUD60x18.mul(
+            liquidityDeltaFixedPoint,
+            PRBMathUD60x18.div(PRBMathUD60x18.toUint(1), priceSqrtCurrentSlotFixedPoint) -
+                PRBMathUD60x18.div(PRBMathUD60x18.toUint(1), priceSqrtUpperSlotFixedPoint)
+        );
 
-function _calculateDeltaY(
-    uint256 liquidityDeltaFixedPoint,
-    uint256 priceSqrtAFixedPoint,
-    uint256 priceSqrtBFixedPoint
-) pure returns (uint256) {
-    (priceSqrtAFixedPoint, priceSqrtBFixedPoint) = priceSqrtAFixedPoint < priceSqrtBFixedPoint
-        ? (priceSqrtAFixedPoint, priceSqrtBFixedPoint)
-        : (priceSqrtBFixedPoint, priceSqrtAFixedPoint);
-
-    return PRBMathUD60x18.mul(liquidityDeltaFixedPoint, priceSqrtBFixedPoint - priceSqrtAFixedPoint);
+        amountB = PRBMathUD60x18.mul(
+            liquidityDeltaFixedPoint,
+            priceSqrtCurrentSlotFixedPoint - priceSqrtLowerSlotFixedPoint
+        );
+    } else {
+        amountB = PRBMathUD60x18.mul(
+            liquidityDeltaFixedPoint,
+            priceSqrtUpperSlotFixedPoint - priceSqrtLowerSlotFixedPoint
+        );
+    }
 }
