7ec6224bb9ca0f856b119d20922aa716fb3edcdf
clemlak <clementlakhal@gmail.com>
2022-11-15 19:07:35 +0400

    feat: add calculate delta X and Y

--- contracts/libraries/BrainMath.sol
+++ contracts/libraries/BrainMath.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity 0.8.13;
 
+// TODO: Check which one is the best to use?
 import "@prb/math/contracts/PRBMathSD59x18.sol";
+import "@prb/math/contracts/PRBMathUD60x18.sol";
 
 /// @dev Get the price square root using the slot index
 ///      $$p(i)=1.0001^i$$
@@ -19,4 +21,32 @@ function _getSlotAtPriceSqrt(uint256 aFixedPoint, uint256 priceSqrtFixedPoint) p
                 PRBMathSD59x18.sqrt(int256(priceSqrtFixedPoint))
             )
         );
+}
+
+function _calculateDeltaX(
+    uint256 liquidityDeltaFixedPoint,
+    uint256 priceSqrtAFixedPoint,
+    uint256 priceSqrtBFixedPoint
+) pure returns (uint256) {
+    (priceSqrtAFixedPoint, priceSqrtBFixedPoint) = priceSqrtAFixedPoint < priceSqrtBFixedPoint
+        ? (priceSqrtAFixedPoint, priceSqrtBFixedPoint)
+        : (priceSqrtBFixedPoint, priceSqrtAFixedPoint);
+
+    return
+        PRBMathUD60x18.div(
+            PRBMathUD60x18.mul(liquidityDeltaFixedPoint, priceSqrtBFixedPoint - priceSqrtAFixedPoint),
+            PRBMathUD60x18.mul(priceSqrtBFixedPoint, priceSqrtAFixedPoint)
+        );
+}
+
+function _calculateDeltaY(
+    uint256 liquidityDeltaFixedPoint,
+    uint256 priceSqrtAFixedPoint,
+    uint256 priceSqrtBFixedPoint
+) pure returns (uint256) {
+    (priceSqrtAFixedPoint, priceSqrtBFixedPoint) = priceSqrtAFixedPoint < priceSqrtBFixedPoint
+        ? (priceSqrtAFixedPoint, priceSqrtBFixedPoint)
+        : (priceSqrtBFixedPoint, priceSqrtAFixedPoint);
+
+    return PRBMathUD60x18.mul(liquidityDeltaFixedPoint, priceSqrtBFixedPoint - priceSqrtAFixedPoint);
 }
