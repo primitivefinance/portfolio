75e415a05ee5ed89759db9a933d2a6065fc461d5
clemlak <clementlakhal@gmail.com>
2022-11-30 21:38:27 +0400

    add pure func modifier

--- contracts/libraries/BrainMath.sol
+++ contracts/libraries/BrainMath.sol
@@ -76,7 +76,7 @@ function getXMaxToNextSlot(
     uint256 sqrtPriceCurrentSlotFixedPoint,
     uint256 sqrtPriceNextSlotFixedPoint,
     uint256 liquidity
-) returns (uint256) {
+) pure returns (uint256) {
     return PRBMathUD60x18.div(PRBMathUD60x18.toUint(liquidity), sqrtPriceNextSlotFixedPoint) - PRBMathUD60x18.div(PRBMathUD60x18.toUint(liquidity), sqrtPriceCurrentSlotFixedPoint);
 }
 
@@ -84,7 +84,7 @@ function getYMaxToNextSlot(
     uint256 sqrtPriceCurrentSlotFixedPoint,
     uint256 sqrtPriceNextSlotFixedPoint,
     uint256 liquidity
-) returns (uint256) {
+) pure returns (uint256) {
     return PRBMathUD60x18.mul(PRBMathUD60x18.toUint(liquidity), sqrtPriceNextSlotFixedPoint - sqrtPriceCurrentSlotFixedPoint);
 }
 
@@ -92,13 +92,13 @@ function getNewPrice(
     uint256 sqrtPriceCurrentSlotFixedPoint,
     uint256 liquidity,
     uint256 deltaX
-) returns (uint256) {
+) pure returns (uint256) {
     return PRBMathUD60x18.div(
         PRBMathUD60x18.mul(
             sqrtPriceCurrentSlotFixedPoint, PRBMathUD60x18.toUint(liquidity)
         ),
         PRBMathUD60x18.mul(
             deltaX, sqrtPriceCurrentSlotFixedPoint
-        ) + PRBMathUD60x18.toUint(liquidity),
+        ) + PRBMathUD60x18.toUint(liquidity)
     );
 }
