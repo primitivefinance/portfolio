1d146e633df7dffac6088658f805569b59a483d5
sam-iamm <supersmitchell3@gmail.com>
2022-12-05 22:07:06 -0800

    refactor tests to use real-world numbers

--- test/foundry/Hyper.t.sol
+++ test/foundry/Hyper.t.sol
@@ -3,64 +3,124 @@ pragma solidity 0.8.13;
 import "forge-std/Test.sol";
 import "forge-std/Vm.sol";
 
-import {UD60x18, fromUD60x18, toUD60x18, ud} from "@prb/math/UD60x18.sol";
-
 import "../../contracts/Hyper.sol";
-import {getPoolId} from "../../contracts/libraries/Pool.sol";
 import "../../contracts/test/TestERC20.sol";
 
 contract TestHyper is Test {
     Hyper public hyper;
+
     TestERC20 public tokenA;
     TestERC20 public tokenB;
 
-    address alice = vm.addr(0xbeef);
+    address testUser = vm.addr(0xbeef);
+    address auctionCollector = vm.addr(0xbabe);
+
+    modifier mintApproveTokens() {
+        // mint tokens to the test user
+        tokenA.mint(testUser, 1000000 ether);
+        tokenA.approve(address(hyper), type(uint256).max);
+        tokenB.mint(testUser, 1000000 ether);
+        tokenB.approve(address(hyper), type(uint256).max);
+        _;
+    }
 
     function setUp() public {
-        hyper = new Hyper(1000, address(alice));
-        tokenA = new TestERC20("TokenA", "TA", 18);
-        tokenB = new TestERC20("TokenB", "TB", 18);
+        hyper = new Hyper(1000, auctionCollector);
 
         vm.warp(1000);
         hyper.start();
-        vm.startPrank(alice);
+
+        vm.startPrank(testUser);
+
+        TestERC20 fakeUSDC = new TestERC20("USD Coin", "USDC", 6);
+        TestERC20 fakeWETH = new TestERC20("Wrapped Ether", "WETH", 18);
+
+        (tokenA, tokenB) = address(fakeUSDC) < address(fakeWETH) ? (fakeUSDC, fakeWETH) : (fakeWETH, fakeUSDC);
+
+        assertTrue(tokenA == fakeUSDC);
     }
 
-    function test_activatePool_should_work() public {
-        hyper.activatePool(address(tokenA), address(tokenB), toUD60x18(10));
+    function getStartSqrtPrice() public pure returns (UD60x18 sqrtPrice) {
+        UD60x18 ethPrice = toUD60x18(1200);
+        UD60x18 usdPrice = ethPrice.inv();
+        // decimals factor = 10^(tokenB.decimals - tokenA.decimals) = 1e12
+        UD60x18 usdcPrice = usdPrice.mul(toUD60x18(1e12));
+        sqrtPrice = usdcPrice.sqrt();
     }
 
-    function test_activatePool_should_fail() public {
-        hyper.activatePool(address(tokenA), address(tokenB), toUD60x18(10));
+    function test_activatePool_succeeds() public {
+        hyper.activatePool(address(tokenA), address(tokenB), getStartSqrtPrice());
+    }
+
+    function test_activatePool_duplication_reverts() public {
+        hyper.activatePool(address(tokenA), address(tokenB), getStartSqrtPrice());
         vm.expectRevert(PoolAlreadyInitializedError.selector);
-        hyper.activatePool(address(tokenA), address(tokenB), toUD60x18(10));
+        hyper.activatePool(address(tokenA), address(tokenB), getStartSqrtPrice());
     }
 
-    function test_add_liquidity_above_current_slot() public {
-        hyper.activatePool(address(tokenA), address(tokenB), toUD60x18(10));
-        tokenA.mint(alice, 1000000 ether);
-        tokenA.approve(address(hyper), type(uint256).max);
-        tokenB.mint(alice, 1000000 ether);
-        tokenB.approve(address(hyper), type(uint256).max);
-
-        hyper.updateLiquidity(getPoolId(address(tokenA), address(tokenB)), 10, 12, int256(100), false);
+    function test_updateLiquidity_add_above_current_slot_succeeds() public mintApproveTokens {
+        // activate pool
+        hyper.activatePool(address(tokenA), address(tokenB), getStartSqrtPrice());
+        // get pool id
+        bytes32 poolId = getPoolId(address(tokenA), address(tokenB));
+        // fetch activated pool
+        (, , , , , , int128 slotIndex, , , , ) = hyper.pools(poolId);
+        // set position range params
+        int128 lowerSlotIndex = slotIndex + 10;
+        int128 upperSlotIndex = lowerSlotIndex + 10;
+        // don't transfer out (shouldn't matter here)
+        bool transferOut = false;
+        // finally, add liquidity
+        hyper.updateLiquidity(
+            getPoolId(address(tokenA), address(tokenB)),
+            lowerSlotIndex,
+            upperSlotIndex,
+            int256(100),
+            transferOut
+        );
     }
 
-    function test_add_liquidity_including_current_slot() public {
-        hyper.activatePool(address(tokenA), address(tokenB), toUD60x18(10));
-        tokenA.mint(alice, 1000000 ether);
-        tokenA.approve(address(hyper), type(uint256).max);
-        tokenB.mint(alice, 1000000 ether);
-        tokenB.approve(address(hyper), type(uint256).max);
-        hyper.updateLiquidity(getPoolId(address(tokenA), address(tokenB)), 9, 11, int256(1000000000000000000), false);
+    function test_updateLiquidity_add_including_current_slot_succeeds() public mintApproveTokens {
+        // activate pool
+        hyper.activatePool(address(tokenA), address(tokenB), getStartSqrtPrice());
+        // get pool id
+        bytes32 poolId = getPoolId(address(tokenA), address(tokenB));
+        // fetch activated pool
+        (, , , , , , int128 slotIndex, , , , ) = hyper.pools(poolId);
+        // set position range params
+        int128 lowerSlotIndex = slotIndex - 10;
+        int128 upperSlotIndex = slotIndex + 10;
+        // don't transfer out (shouldn't matter here)
+        bool transferOut = false;
+        // finally, add liquidity
+        hyper.updateLiquidity(
+            getPoolId(address(tokenA), address(tokenB)),
+            lowerSlotIndex,
+            upperSlotIndex,
+            int256(100),
+            transferOut
+        );
     }
 
-    function test_add_liquidity_below_current_slot() public {
-        hyper.activatePool(address(tokenA), address(tokenB), toUD60x18(10));
-        tokenA.mint(alice, 1000000 ether);
-        tokenA.approve(address(hyper), type(uint256).max);
-        tokenB.mint(alice, 1000000 ether);
-        tokenB.approve(address(hyper), type(uint256).max);
-        hyper.updateLiquidity(getPoolId(address(tokenA), address(tokenB)), 8, 9, int256(1), false);
+    function test_updateLiquidity_add_below_current_slot_succeeds() public mintApproveTokens {
+        // activate pool
+        hyper.activatePool(address(tokenA), address(tokenB), getStartSqrtPrice());
+        // get pool id
+        bytes32 poolId = getPoolId(address(tokenA), address(tokenB));
+        // fetch activated pool
+        (, , , , , , int128 slotIndex, , , , ) = hyper.pools(poolId);
+        // set position range params
+        int128 lowerSlotIndex = slotIndex - 100;
+        int128 upperSlotIndex = slotIndex - 10;
+        // don't transfer out (shouldn't matter here)
+        bool transferOut = false;
+        // finally, add liquidity
+        hyper.updateLiquidity(
+            getPoolId(address(tokenA), address(tokenB)),
+            lowerSlotIndex,
+            upperSlotIndex,
+            int256(100),
+            transferOut
+        );
     }
 }
