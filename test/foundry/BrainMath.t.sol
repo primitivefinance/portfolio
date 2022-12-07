7dc38d65347dc36b43590f321d737b30cec6f12f
clemlak <clementlakhal@gmail.com>
2022-12-07 13:21:07 +0400

    add MAX_PERCENT_DELTA and approx tests

--- test/foundry/BrainMath.t.sol
+++ test/foundry/BrainMath.t.sol
@@ -7,6 +7,8 @@ import {UD60x18, fromUD60x18, toUD60x18, ud, wrap, unwrap} from "@prb/math/UD60x
 import "../../contracts/libraries/BrainMath.sol";
 
 contract TestBrainMath is Test {
+    uint256 constant MAX_PERCENT_DELTA = 0.000000001e18;
+
     function test_sqrtPriceGridBase() public {
         UD60x18 sqrtPriceGridBase = wrap(PRICE_GRID_BASE).sqrt();
         assertEq(unwrap(sqrtPriceGridBase), SQRT_PRICE_GRID_BASE);
@@ -17,17 +19,38 @@ contract TestBrainMath is Test {
         assertEq(unwrap(lnSqrtPriceGridBase), LN_SQRT_PRICE_GRID_BASE);
     }
 
-    function test_sqrtPriceAtSlotZero() public {
+    function test_getSqrtPriceAtSlot_zero() public {
         int128 slotIndex = 0;
         UD60x18 sqrtPrice = _getSqrtPriceAtSlot(slotIndex);
         assertEq(unwrap(sqrtPrice), unwrap(toUD60x18(1)));
-        assertEq(unwrap(sqrtPrice), 1e18);
     }
 
-    function test_sqrtPriceAtSlotPositiveIndex() public {
+    function test_getSqrtPriceAtSlot_positive_index() public {
         int128 slotIndex = 1000;
         UD60x18 sqrtPrice = _getSqrtPriceAtSlot(slotIndex);
         emit log_uint(unwrap(sqrtPrice));
+        assertApproxEqRel(
+            unwrap(sqrtPrice),
+            1051268468376662000,
+            MAX_PERCENT_DELTA
+        );
+    }
+
+    function test_getSqrtPriceAtSlot_negative_index() public {
+        int128 slotIndex = -1000;
+        UD60x18 sqrtPrice = _getSqrtPriceAtSlot(slotIndex);
+        emit log_uint(unwrap(sqrtPrice));
+        assertApproxEqRel(
+            unwrap(sqrtPrice),
+            951231802418815800,
+            MAX_PERCENT_DELTA
+        );
+    }
+
+    function test_getSlotAtSqrtPrice_random_price() public {
+        UD60x18 sqrtPrice = toUD60x18(1200).sqrt();
+        int128 slotIndex = _getSlotAtSqrtPrice(sqrtPrice);
+        assertEq(slotIndex, 70904);
     }
 
     function test_calculateLiquidityUnderlying_should_round_down() public {
