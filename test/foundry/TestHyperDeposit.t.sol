79021d78d4c2b83cb6b3499b745489b0ab83b737
clemlak <clementlakhal@gmail.com>
2022-12-28 12:26:18 +0100

    test: checks Deposit, IncreaseUserBalance events

--- test/foundry/TestHyperDeposit.t.sol
+++ test/foundry/TestHyperDeposit.t.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity ^0.8.0;
 
+import "forge-std/Vm.sol";
 import "./setup/TestHyperSetup.sol";
 
 contract TestHyperDeposit is TestHyperSetup {
@@ -13,5 +14,21 @@ contract TestHyperDeposit is TestHyperSetup {
 
         assertTrue(nextBalance < prevBalance);
         assertTrue(nextWethBalance > prevWethBalance);
+    }
+
+    event Deposit(address indexed account, uint amount);
+
+    function testDepositWrapsEther_emit_Deposit() public {
+        vm.expectEmit(true, true, false, true);
+        emit Deposit(address(this), 4000);
+        __hyperTestingContract__.deposit{value: 4000}();
+    }
+
+    event IncreaseUserBalance(address indexed token, uint256 amount);
+
+    function testDepositWrapsEther_emit_IncreaseUserBalance() public {
+        vm.expectEmit(true, true, false, true);
+        emit IncreaseUserBalance(address(__weth__), 4000);
+        __hyperTestingContract__.deposit{value: 4000}();
     }
 }
