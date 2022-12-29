4c5d0598281c8da1e4e408b241e5e651ac2d7ff8
clemlak <clementlakhal@gmail.com>
2022-12-30 00:24:28 +0100

    test: add testSwap_revert_ZeroInput

--- test/foundry/TestHyperSwap.t.sol
+++ test/foundry/TestHyperSwap.t.sol
@@ -3,4 +3,19 @@ pragma solidity ^0.8.0;
 
 import "./setup/TestHyperSetup.sol";
 
-contract TestHyperSwap is TestHyperSetup {}
+contract TestHyperSwap is TestHyperSetup {
+    modifier allocateFirst() {
+        allocatePool(address(__hyperTestingContract__), defaultScenario.poolId, 10e19);
+        _;
+    }
+
+    function testSwap_revert_ZeroInput() public {
+        vm.expectRevert(ZeroInput.selector);
+        __hyper__.swap(
+            defaultScenario.poolId,
+            true,
+            0,
+            0
+        );
+    }
+}
