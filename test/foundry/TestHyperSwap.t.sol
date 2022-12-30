7c00fc524fd2a9b3a9f70c8c7660b775294fc430
clemlak <clementlakhal@gmail.com>
2022-12-30 12:53:18 +0100

    test: add new swap tests

--- test/foundry/TestHyperSwap.t.sol
+++ test/foundry/TestHyperSwap.t.sol
@@ -10,6 +10,55 @@ contract TestHyperSwap is TestHyperSetup {
     }
 
     function testSwap_should_succeed() public allocateFirst() {
+        (uint output, uint remainder) = __hyperTestingContract__.swap(
+            defaultScenario.poolId,
+            false,
+            10000,
+            type(uint256).max
+        );
+    }
+
+    function testSwap_back_and_forth_outputs_less() public allocateFirst() {
+        uint256 start = 10000;
+
+        (uint output, ) = __hyperTestingContract__.swap(
+            defaultScenario.poolId,
+            false,
+            start,
+            type(uint256).max
+        );
+
+        (uint finalOutput, ) = __hyperTestingContract__.swap(
+            defaultScenario.poolId,
+            true,
+            output,
+            type(uint256).max
+        );
+
+        assertGt(start, finalOutput);
+    }
+
+    function testSwap_revert_PoolExpiredError() public allocateFirst() {
+        (
+            uint256 lastPrice,
+            int24 lastTick,
+            uint256 blockTimestamp,
+            uint256 liquidity,
+            uint256 stakedLiquidity,
+            uint256 borrowableLiquidity,
+            int256 epochStakedLiquidityDelta,
+            address prioritySwapper,
+            uint256 priorityPaymentPerSecond,
+            uint256 feeGrowthGlobalAsset,
+            uint256 feeGrowthGlobalQuote
+        ) = __hyperTestingContract__.pools(
+            defaultScenario.poolId
+        );
+
+        vm.warp(blockTimestamp + __hyperTestingContract__.BUFFER());
+
+        vm.expectRevert(PoolExpiredError.selector);
+
         __hyperTestingContract__.swap(
             defaultScenario.poolId,
             false,
