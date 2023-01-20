55233b7d9bf89c85e3c7c7e65130eda287ff48ff
Waylon Jepsen <waylonjepsen1@gmail.com>
2023-01-20 10:49:33 -0700

    integrate foundry contracts and imports

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,20 +1,18 @@
-use std::{env, sync::Arc};
+use std::{sync::Arc};
 // use tokio::*;
 use eyre::Result;
 // use ethers::providers::{Http, Provider};
-use ethers::{abi::Address, prelude::*, providers::Provider};
-use bindings::*;
+use ethers::{abi::Address, prelude::*, providers::{Provider,Middleware}};
+use bindings::hyper;
 
 // Need to get the correct address for the contract
 const FACTORY: &str = "0x1F98431c8aD98523631AE4a59f267346ea31F984";
 #[tokio::main]
 async fn main() -> Result<()> {
-    let provider = match env::var_os("PROVIDER") {
-        Some(v) => Arc::new(Provider::<Http>::try_from(v.into_string().unwrap())?),
-        None => get_provider().await,
-    };
+    let provider = get_provider().await;
     let hyper_address = FACTORY.parse::<Address>().unwrap();
     let hyper = bindings::hyper::Hyper::new(hyper_address, provider);
+    // let call = hyper.get_version().call().await?;
     Ok(())
 
 }
