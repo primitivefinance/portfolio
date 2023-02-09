68c722593ae99bca5eacfb8580b7df2120d851cd
clemlak <clementlakhal@gmail.com>
2023-02-09 14:58:22 +0400

    test: comment out unused line of code

--- contracts/echidna/EchidnaE2E.sol
+++ contracts/echidna/EchidnaE2E.sol
@@ -1532,7 +1532,7 @@ contract EchidnaE2E is HelperHyperView, Helper, EchidnaStateHandling {
     function swap_quote_in_increases_reserve(uint id, bool sellAsset, uint256 amount, uint256 limit) public {
         require(!sellAsset);
 
-        address[] memory owners = new address[](1);
+        // address[] memory owners = new address[](1);
         // Will always return a pool that exists
         (
             HyperPool memory pool,
