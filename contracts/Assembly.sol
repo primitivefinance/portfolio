a9f875f8c821d6a79e2d8bd3e580a943aa9e4249
clemlak <clementlakhal@gmail.com>
2023-02-09 12:28:34 +0400

    feat: remove unused __between

--- contracts/Assembly.sol
+++ contracts/Assembly.sol
@@ -39,17 +39,6 @@ function isBetween(uint256 value, uint256 lower, uint256 upper) pure returns (bo
     }
 }
 
-function __between(int256 value, int256 lower, int256 upper) pure returns (bool valid) {
-    assembly {
-        // Is `val` btwn lo and hi, inclusive?
-        function isValid(val, lo, hi) -> btwn {
-            btwn := iszero(sgt(mul(sub(val, lo), sub(val, hi)), 0)) // iszero(x > amount ? 1 : 0) ? true : false, (n - a) * (n - b) <= 0, n = amount, a = lower, b = upper
-        }
-
-        valid := isValid(value, lower, upper)
-    }
-}
-
 /**
 
     @dev Reference:
