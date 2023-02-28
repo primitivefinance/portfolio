a6f4315c4f89d38193a8e3d47e209944b72db98d
clemlak <clementlakhal@gmail.com>
2023-02-28 16:57:27 +0400

    test: add TestAssemblyLib

---
+++ test/TestAssemblyLib.t.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.4;
+
+import "forge-std/Test.sol";
+import "forge-std/Vm.sol";
+import "contracts/libraries/AssemblyLib.sol";
+
+contract TestAssemblyLib is Test {
+    function test_fromAmount() public {
+        {
+            (uint8 power, uint128 base) = fromAmount(500);
+            assertEq(power, 2);
+            assertEq(base, 5);
+        }
+    }
+}
