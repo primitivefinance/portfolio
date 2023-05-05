06623301092c6e61e5b71136378fc91e84b9cb59
clemlak <clementlakhal@gmail.com>
2023-05-05 14:54:43 +0400

    feat: remove unused toBytes8

--- contracts/libraries/AssemblyLib.sol
+++ contracts/libraries/AssemblyLib.sol
@@ -126,25 +126,6 @@ library AssemblyLib {
     }
 
     /**
-     * @dev There's no explict casting from dynamic to fixed sized bytes, this function
-     * handles it for us.
-     */
-    function toBytes8(bytes memory raw) internal pure returns (bytes8 data) {
-        bytes4 errorSelector = DataTooLong.selector;
-
-        assembly {
-            if gt(mload(raw), 8) {
-                mstore(0, errorSelector)
-                revert(0, 4)
-            }
-
-            data := mload(add(raw, 32))
-            let shift := mul(sub(8, mload(raw)), 8)
-            data := shr(shift, data)
-        }
-    }
-
-    /**
      * @dev Safely casts an unsigned 128-bit integer into a signed 128-bit integer.
      * Reverts on overflow.
      */
