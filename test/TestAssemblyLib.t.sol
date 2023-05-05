5b043baea3ac87db87e89a4e73468e2d24e7c825
clemlak <clementlakhal@gmail.com>
2023-05-05 14:55:46 +0400

    test: remove separate tests

--- test/TestAssemblyLib.t.sol
+++ test/TestAssemblyLib.t.sol
@@ -55,15 +55,6 @@ contract TestAssemblyLib is Test {
         );
     }
 
-    function testFuzz_separate(uint8 a, uint8 b) public {
-        vm.assume(a <= 15);
-        vm.assume(b <= 15);
-        bytes1 data = AssemblyLib.pack(bytes1(a), bytes1(b));
-        (bytes1 a_, bytes1 b_) = AssemblyLib.separate(data);
-        assertEq(a, uint8(a_));
-        assertEq(b, uint8(b_));
-    }
-
     /*
     function test_trimBytes() public {
         bytes memory output = AssemblyLib.trimBytes(abi.encode(2));
