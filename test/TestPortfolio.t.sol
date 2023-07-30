4bf82d0669aeaeaa9a8d3abd176a8ce607527d2f
alex <alexangel.j@gmail.com>
2023-07-29 23:51:40 -0700

    fix(sprbt-43): improves reentrancy guards

--- test/TestPortfolio.t.sol
+++ test/TestPortfolio.t.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.4;
 
 import "./Setup.sol";
+import "../contracts/libraries/SafeTransferLib.sol" as TransferLib;
 
 contract TestPortfolio is Setup {
     uint256 constant LOCKED_SLOT = 12; // `$ forge inspect Portfolio storageLayout`
@@ -53,5 +54,106 @@ contract TestPortfolio is Setup {
 
         uint24 expected = PoolId.wrap(encoded).pairId();
         assertEq(expected, uint24(encoded >> 32), "pair id method");
+    }
+
+    address internal _malicious;
+
+    modifier makeMaliciousToken() {
+        _malicious = address(new MaliciousReentrancyToken());
+        _;
+    }
+
+    /// @dev To set this test up, we need to inject a single-call re-entrancy
+    /// using the `transferFrom` function on a malicious token.
+    /// This will get triggered during settlement.
+    /// multicall
+    ///     preLock()
+    ///         allocate()
+    ///     postLock()
+    ///     settlement()
+    ///        transferFrom()
+    ///            multicall()
+    ///                preLock()
+    /// ...             should revert here.
+    function test_reentrancy_duing_multicall_settlement()
+        public
+        makeMaliciousToken
+        customTokenConfig(true, _malicious)
+        useActor
+        usePairTokens(100 ether)
+    {
+        bytes[] memory instructions = new bytes[](1);
+        instructions[0] = abi.encodeCall(
+            IPortfolioActions.allocate,
+            (
+                false,
+                address(this),
+                ghost().poolId,
+                1 ether, // make sure we can provide enough liquidity...
+                type(uint128).max,
+                type(uint128).max
+            )
+        );
+
+        // Send some tokens to the malicious token contract so it can allocate...
+        ghost().asset().to_token().transfer(_malicious, 25 ether);
+        ghost().quote().to_token().transfer(_malicious, 25 ether);
+
+        IPortfolio subject_ = subject();
+
+        // The call originally reverts with `Portfolio_InvalidInvariant`, however...
+        // because the external call was `safeTransferFrom`, the revert is caught
+        // and the `TokenTransferFrom` error is thrown instead.
+        // Pass the -vvv flag to the test command to see the revert message.
+        // forge test -vvv --match-test test_reentrancy_duing_multicall_settlement
+        vm.expectRevert(TransferLib.TokenTransferFrom.selector);
+        subject_.multicall(instructions);
+    }
+}
+
+contract MaliciousReentrancyToken is ERC20, ERC1155TokenReceiver {
+    constructor() ERC20("MaliciousReentrancy", "MUTEX", 18) {
+        if (block.chainid == 1) revert("bad");
+    }
+
+    uint64 internal _poolId;
+
+    function set(uint64 poolId) external {
+        _poolId = poolId;
+    }
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+
+    /// @dev Malicious re-entrancy in untrusted & dangerous `transferFrom` call in settlement.
+    /// Portfolio.settlement
+    ///    this.transferFrom()
+    ///        msg.sender.multicall()
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) public override returns (bool) {
+        // Re-enter here.
+        bytes[] memory instructions = new bytes[](1);
+        instructions[0] = abi.encodeCall(
+            IPortfolioActions.allocate,
+            (
+                false,
+                address(this),
+                _poolId,
+                1 ether, // make sure we can provide enough liquidity...
+                type(uint128).max,
+                type(uint128).max
+            )
+        );
+
+        // Re-enter with multicall!
+        IPortfolio(msg.sender).multicall(instructions);
+
+        // Call the regular logic..
+        super.transferFrom(from, to, amount);
+        return true;
     }
 }
