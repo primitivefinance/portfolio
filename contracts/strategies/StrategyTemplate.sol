dc3695015d6295bfcb61870257d75daee568102d
clemlak <clementlakhal@gmail.com>
2023-11-22 18:23:05 +0400

    chore: add NatSpec to StrategyTempalte

--- contracts/strategies/StrategyTemplate.sol
+++ contracts/strategies/StrategyTemplate.sol
@@ -3,15 +3,25 @@ pragma solidity 0.8.19;
 
 import "../interfaces/IStrategy.sol";
 
+/**
+ * @title Portfolio Strategy Template
+ * @author Primitive™
+ * @notice This abstract contract can be extended to create custom strategies
+ * for Portfolio.
+ */
 abstract contract StrategyTemplate is IStrategy {
+    /// @dev Thrown when the sender is not the Portfolio contract.
     error NotPortfolio();
 
+    /// @dev Address of the Portfolio contract.
     address public immutable portfolio;
 
+    /// @param portfolio_ Address of the Portfolio contract.
     constructor(address portfolio_) {
         portfolio = portfolio_;
     }
 
+    /// @dev Modifier to check that the sender is the Portfolio contract.
     modifier onlyPortfolio() {
         if (msg.sender != portfolio) revert NotPortfolio();
         _;
